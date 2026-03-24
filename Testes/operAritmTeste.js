7a83ee8aad62a55ce521b853c28f2a9923cbe2e3
FELIPE BUTTENDORF DAMA <felipe_dama@estudante.sesisenai.org.br>
2026-03-23 21:27:47 -0300

    Atualizando

---
+++ Testes/operAritmTeste.js
@@ -0,0 +1,21 @@
+let a = 3
+let b = 7
+console.log(a+b)
+console.log(b-a)
+console.log(a*b)
+console.log(b/a)
+console.log(b%a)
+console.log(a**b)
+console.log(a**0.5)
+console.log(5 * "batata"); // NaN → Not a Number
+console.log(5 / 0);
+let valor = 15.99
+console.log(Math.round(valor))
+console.log(Math.floor(valor))
+console.log(Math.ceil(valor))
+let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(Math.max(...numeros)); // Resultado: 9
+console.log(Math.min(...numeros)); // Resultado: 0
+let decimal = 13.45
+console.log(Math.abs(decimal))
+console.log(decimal.toFixed(2))
