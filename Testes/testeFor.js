aa3e593ad1a869d810ac25c5165bc3e3aa8357f7
FELIPE BUTTENDORF DAMA <felipe_dama@estudante.sesisenai.org.br>
2026-04-27 21:34:53 -0300

    Atualizando jogos desafios

--- Testes/testeFor.js
+++ Testes/testeFor.js
@@ -1,5 +1,9 @@
 
 let lerTeclado = require("readline-sync");
-for(let repetir = true; repetir ; repetir ) {
-    repetir = lerTeclado.keyInYN(`Gostaria de repetir o código?`)
+// for(let repetir = true; repetir ; repetir ) {
+//     repetir = lerTeclado.keyInYN(`Gostaria de repetir o código?`)
+// }
+let num = null
+for (num = lerTeclado.keyInYN(`Digite um número :`); num !== 0; num / 2 ) {
+    if(num )
 }
