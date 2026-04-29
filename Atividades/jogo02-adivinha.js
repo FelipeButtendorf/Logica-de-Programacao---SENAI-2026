cabe56db3e983394d1dea064f2fd6cc5f3b8dcc9
FELIPE BUTTENDORF DAMA <felipe_dama@estudante.sesisenai.org.br>
2026-04-28 21:53:22 -0300

    Atualizando atividades for

--- Atividades/jogo02-adivinha.js
+++ Atividades/jogo02-adivinha.js
@@ -25,23 +25,10 @@ let lerTeclado = require("readline-sync");
 // CONFIGURAÇÕES DO JOGO (não altere)
 // ============================================================
 
-const MAX_TENTATIVAS = 7;
-const NUM_MIN = 1;
-const NUM_MAX = 100;
 
 // ============================================================
 // MENU PRINCIPAL
 // ============================================================
-
-console.log("|==============================|");
-console.log("|     ADIVINHE O NÚMERO        |");
-console.log("|==============================|");
-console.log("|  1 – Eu adivinho o número    |");
-console.log("|  2 – Computador adivinha     |");
-console.log("|  3 – Sair                    |");
-console.log("|==============================|");
-
-const modo = lerTeclado.questionInt("\nEscolha o modo: ");
 
 console.log("_______________________________");
 
@@ -65,32 +52,6 @@ console.log("_______________________________");
 //           "Resultado: Vitória" ou "Resultado: Derrota"
 
 // → Seu código do MODO 1 aqui:
-let tentativas = 1;
-switch (modo) {
-  case 1:
-    const secreto = Math.floor(Math.random() * (100 + 1));
-    for (let i = 0; i = 7; i++) {
-      let chute = lerTeclado.questionInt(`Chute um número:`);
-      if (chute === secreto && i === 0) {
-        console.log(`Parabéns! Você acertou em 1 tentativa!
-            Número secreto:${secreto}, Tentativas usadas: ${tentativas}, Resultado: Vitória`);
-            process.exit()
-      } else if (chute === secreto) {
-        console.log(`Parabéns! Você acertou em ${tentativas} tentativa(s)!
-            Número secreto:${secreto}, Tentativas usadas: ${tentativas}, Resultado: Vitória`);
-            process.exit()
-      } else if (chute > secreto) {
-        console.log(`Muito alto! Tente um número menor.`);
-      } else if (chute < secreto) {
-        console.log(`Muito baixo! Tente um número maior.`);
-      }
-      console.log(`Restam ${7 - tentativas} tentativas.`);
-      tentativas++;
-    }
-    console.log(`Fim de jogo! O número era ${secreto}.
-    Número secreto:${secreto}, Tentativas usadas: ${tentativas}, Resultado: Derrota`)
-    break;
-}
 
 // ============================================================
 // MODO 2 – Jogador pensa, computador adivinha
@@ -112,3 +73,85 @@ switch (modo) {
 // PASSO 5 – Se o computador não acertar, exiba uma mensagem de derrota e o número que o jogador estava pensando.
 
 // Implemente seu código abaixo:
+
+let NUM_MIN = 1;
+let NUM_MAX = 100;
+
+
+console.log("|==============================|");
+console.log("|     ADIVINHE O NÚMERO        |");
+console.log("|==============================|");
+console.log("|  1 – Eu adivinho o número    |");
+console.log("|  2 – Computador adivinha     |");
+console.log("|  3 – Sair                    |");
+console.log("|==============================|");
+
+const modo = lerTeclado.questionInt("\nEscolha o modo: ");
+
+let tentativas = 1;
+
+switch (modo) {
+  case 1:
+    const secreto = Math.floor(Math.random() * (100 + 1));
+    for (let i = 0; i <= 6; i++) {
+      let chute = lerTeclado.questionInt(`Chute um número:`);
+      if (chute === secreto && i === 0) {
+        console.log(`Parabéns! Você acertou em 1 tentativa!
+            Número secreto:${secreto}, Tentativas usadas: ${tentativas}, Resultado: Vitória`);
+        process.exit();
+      } else if (chute === secreto) {
+        console.log(`Parabéns! Você acertou em ${tentativas} tentativa(s)!
+            Número secreto:${secreto}, Tentativas usadas: ${tentativas}, Resultado: Vitória`);
+        process.exit();
+      } else if (chute > secreto) {
+        console.log(`Muito alto! Tente um número menor.`);
+      } else if (chute < secreto) {
+        console.log(`Muito baixo! Tente um número maior.`);
+      }
+      console.log(`Restam ${7 - tentativas} tentativas.`);
+      tentativas++;
+    }
+    console.log(`Fim de jogo! O número era ${secreto}.
+    Número secreto:${secreto}, Tentativas usadas: ${tentativas}, Resultado: Derrota`);
+    break;
+  case 2:
+    let tentativasPC = 1;
+    let palpite = Math.floor(Math.random() * (100 + 1));
+    let N = lerTeclado.questionInt(`Digite um número entre 1 e 100.
+Digite sua resposta:`);
+    let min = NUM_MIN;
+    let max = NUM_MAX;
+    let novoPalpite = 0;
+    for (let i = 0; i <= 6; i++) {
+      console.log(`Tentativa ${tentativasPC}, Meu palpite é ${palpite}`);
+      let verificacao = lerTeclado.questionInt(`1 – Acertei | 2 – Muito alto | 3 – Muito baixo |
+  Digite sua resposta:`);
+      switch (verificacao) {
+        case 1:
+          console.log(
+            `Parabéns! O computador acertou o número que você escolheu em ${tentativasPC} tentativas`
+          );
+          process.exit();
+          break;
+        case 2:
+          NUM_MAX = palpite
+          palpite = Number(((palpite + NUM_MIN) / 2).toFixed());
+          break;
+        case 3:
+          NUM_MIN = palpite
+          palpite = Number(((palpite + NUM_MAX) / 2).toFixed());
+          break;
+        default:
+          console.log("Número inválido. Saindo... ");
+      }
+      tentativasPC++;
+    }
+    console.log(`Derrota :( O computador não conseguiu acertar o seu número.)`)
+    process.exit
+    break;
+  case 3:
+    console.log("Até outra hora. Saindo... ");
+    break;
+  default:
+    console.log("Número inválido. Saindo... ");
+}
