b64ab5fa1299e776d6a81056d2123c39abbcd76a
FELIPE BUTTENDORF DAMA <felipe_dama@estudante.sesisenai.org.br>
2026-06-09 21:56:08 -0300

    Atualizando blackjack utilizando API

--- Atividades/Jogos/Blackjack.js
+++ Atividades/Jogos/Blackjack.js
@@ -1,31 +1,91 @@
-const deck = [
-    "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
-    "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
-    "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
-    "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC"
-  ]
-const maoDealer = []
-const maoJojador = []
-let cartaAleatoria = null
-function exibirDeck() {
-    console.log(deck)
-}
-function exibirCarta() {
-    console.log(cartaAleatoria)
-    console.log(deck[cartaAleatoria - 1])
-}
-function pegarCartaDoDeck() {
-    cartaAleatoria = Math.floor(Math.random() * (deck.length + 1))
-}
-function adicionarCartaNaMao(mao) {
-    mao.push(deck[cartaAleatoria - 1])
-    deck.splice((cartaAleatoria - 1), 1 )
-}
-pegarCartaDoDeck()
-exibirCarta()
-adicionarCartaNaMao(maoDealer)
-exibirDeck()
-console.log(maoDealer)
+const axios = require(`axios`)
+const rs = require('readline-sync')
+let jogadores = [] // ARRAY DE JOGADORES
+let baralhoID = null
+let cartasCompradas = []
 
+function exibirTodasAsMaos() {
+    for(let i = 0; i < jogadores.length; i++) {
+        console.log(`Mao de ${jogadores[i].nome} : ${jogadores[i].mao}`)
+    }
+} 
 
+function exibirMao(indiceJogador) {
+    console.log(`Mao de ${jogadores[indiceJogador].nome} : ${jogadores[indiceJogador].mao}`)
+} 
 
+function exibirJogadores() {
+    for(let i = 0; i < jogadores.length; i++) {
+        console.log(jogadores[i].nome)
+    }
+}
+
+function exibirJogador(indiceJogador) {
+        console.log(jogadores[indiceJogador].nome)
+}
+
+async function criarBaralho() { // PEGA O ID DE UM BARALHO EMBARALHADO USANDO API E COLOCA NA VÁRIVEL "BaralhoID"
+    try {
+        const resposta = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        baralhoID = resposta.data.deck_id
+    }   
+    catch(error) {
+        console.log(error)
+    }
+}
+
+async function pegarCarta(deckID, quantidade) { // PEGA UMA CARTA DO BARALHO
+    try {
+        const resposta = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${quantidade}`)
+        const cartasID = resposta.data.cards
+        for(let i = 0; i < cartasID.length; i++) {
+            cartasCompradas.push(cartasID[i].code)
+        }
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+function adicionarMao(cartas, mao) { // ADICIONA A(S) CARTA(S) RETIRADAS DO BARALHO A MAO DE ALGUM JOGADOR
+    for(let i = 0; i < cartas.length; i++) {
+        mao.push(cartas[i])
+    }
+    cartasCompradas = []
+}
+
+function registrarJogadores() {
+    let adicionar = false
+    do {
+        jogadores.push(
+            jogador = {
+                nome: rs.question(`Qual o seu nome? : `),
+                mao: [],
+                pontos: null
+            }
+        )
+        if(jogadores.length === 4) {
+            console.log(`Limite máximo de jogadores atingido!`)
+            adicionar = false
+        } else {
+        adicionar = rs.keyInYN(`Gostaria de adicionar mais jogadores(Y/N)? : `)
+        }
+    } while (adicionar)
+}
+
+async function distribuirCartas() {
+    for(let i = 0; i < jogadores.length; i++) {
+        await pegarCarta(baralhoID, 2)
+        adicionarMao(cartasCompradas, jogadores[i].mao)
+    }  
+}
+
+async function iniciarJogo() {
+    registrarJogadores()
+    exibirJogadores()
+    await criarBaralho()
+    distribuirCartas()
+    exibirTodasAsMaos()
+}
+
+iniciarJogo()
