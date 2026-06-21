472be69ccdcf914df74f33319a5d4c9d5fb43bcf
Felipe Buttendorf Dama <felipe_dama@estudante.sesisenai.org.br>
2026-06-20 21:40:55 -0300

    Corrige login da conta

--- Atividades/Banco.js
+++ Atividades/Banco.js
@@ -1,43 +1,15 @@
-// let conta = {
-//     limites: {
-//         saque: null,
-//         deposito: null
-//     },
-//     tipo: {
-//         credito: null,
-//         debito: null
-//     },
-//     infoPessoais: {
-//         nome: null,
-//         idade: null,
-//         email: null,
-//         telefone: null,
-//         cep: {
-//             pais: null,
-//             estado: null,
-//             cidade: null
-//         },
-//         cpf: null
-//     },
-//     saldo: null,
-//     bonus: {},
-//     logado: null,
-//     senha: null,
-//     investimentos: {
-//         rendaFixa: null,
-//         rendaVariavel: null
-//     },
-//     histórico: {},
-//     bloqueada: null
-// }
-
 let rs = require("readline-sync");
-let contas = []
+let usuarios = []
+let opcao = null
+const taxas = {}
+let contaUsuario = null
+let tentativasDeSenha = 3
+let acessoConcedido = false
 
 function criarUsuarioBase() {
-    contas.push(
+    usuarios.push(
         {
-            conta: "24135454364",
+            conta: "123",
             limites: {
                 saque: 10000,
                 deposito: 10000,
@@ -49,11 +21,24 @@ function criarUsuarioBase() {
             },
             infoPessoais: {
                 nome: "Felipe",
-                dataDeNascimento: {
-                    dia: "22",
-                    mes: "02",
-                    ano: "2026"
+                sobrenome: "Buttendorf Dama",
+                nomeCompleto: "Felipe Buttendorf Dama",
+                dataNascimento: new Date('2008-02-22'),
+                dadosResponsavel: {
+                nomeCompleto: null,
+                telefone: null,
+                enderecoCompleto: {
+                    cep: null,
+                    estado: null,
+                    cidade: null,
+                    bairro: null,
+                    rua: null,
+                    numero: null,
+                    complemento: null,
                 },
+                parentalidade: null
+                },
+                dependentes: null,
                 email: "felipecodebreaker@email.com",
                 telefone: "(47) 91234-5678",
                 enderecoCompleto: {
@@ -63,10 +48,11 @@ function criarUsuarioBase() {
                     cidade: "Jaraguá do Sul",
                     bairro: "Três Rios do Norte",
                     rua: "Adelino da Silva",
-                    casa: "31"
-
+                    numero: "31",
+                    complemento: null
                 },
-                cpf: "11101001101"
+                cpf: "11101001101",
+                cnpj: null
             },
             saldo: 2000,
             bonus: {},
@@ -82,45 +68,137 @@ function criarUsuarioBase() {
     )
 }
 
-
-const taxas = {}
-
-function logarConta() {
-    let contaEncontrada = false
-    let contaUsuario = null
-    do{
-        let pedirConta = rs.question(`Qual o número da sua conta? : `)
-        for(let i = 0; i < contas.length; i++) {
-            if(contas[i].conta === pedirConta) {
-                contaEncontrada = true
-                contaUsuario = contas[i]
-                break
-            }
-        }
-        if(contaEncontrada === false) {
-            console.log(`Conta ainda não registrada.`)
-        }
-    }while(contaEncontrada === false)
-    let tentativasDeSenha = 3
-    let senhaValida = false
-    do{
-        let pedirSenha = rs.question(`Qual a senha da sua conta? : `) 
-        if(contaUsuario.senha === pedirSenha) {
-            senhaValida = true
-            console.log(`Logando...`)
-        }else{
-            tentativasDeSenha --
-            console.log(`Senha incorreta por favor, tente novamente. Tentativas restantes: ${tentativasDeSenha}`)
-        }
-    }while(senhaValida === false)
+function exibirMenuInicial() {
+    console.clear
+    console.log("=====================================")
+    console.log("    BEM-VINDO AO BANCO DO BOSTIL")
+    console.log("=====================================")
+    console.log("1 - Acessar Conta")
+    console.log("2 - Criar Conta")
+    console.log("0 - Sair")
+    console.log("=====================================")
 }
 
-console.log("=====================================")
-console.log("          BANCO DO BOSTIL")
-console.log("=====================================")
-console.log("Conta:")
-console.log("Senha:")
-console.log("=====================================")
+function validarEscolha(numMin, numMax, valorEscolhido){
+    if(valorEscolhido < numMin || valorEscolhido > numMax) return true
+    return false
+}
 
-criarUsuarioBase()
-logarConta()
+function escolhaDeValor(numMin, numMax){
+    opcao = rs.questionInt(`
+    Digite a opcao desejada : `)
+    while(validarEscolha(numMin, numMax, opcao)){
+        console.log("\nValor invalido, tente novamente");
+        opcao = rs.questionInt("Opcao: ")
+    }
+    return opcao
+}
+
+function criarConta() {
+    criarUsuarioBase()
+    console.clear()
+    console.log(`
+    Usuario ${usuarios[usuarios.length-1].infoPessoais.nomeCompleto} Cadastrado!\n`)
+}
+
+function acessarOpcaoInicial(escolha) {
+    switch(escolha) {
+        case 1:
+            acessarConta()
+            break
+        case 2:
+            criarConta()
+            break
+        case 0:
+            console.log(`Saindo...`)
+            break
+    }
+}
+
+function pedirConta() {
+    console.clear()
+    let pedirConta = rs.question(`Digite o numero da sua conta : `)
+    return pedirConta
+}
+
+function verificarConta(numeroConta) {
+    for(let i = 0; i < usuarios.length; i++) {
+        if(usuarios[i].conta === numeroConta) {
+            contaUsuario = usuarios[i]
+            return console.log(`Conta encontrada!`)
+        }
+    }
+    return console.log(`Conta inválida! Verifique se os digitos estão corretos e tente novamente.`)
+}
+
+function contaBloqueada(usuario) { 
+    if(contaUsuario.bloqueada) {
+        console.log(`Esta conta está bloqueada no momento, tente novamente.`)
+        return true
+    }
+    return false
+}
+
+function pedirSenha() {
+    let pedirSenha = rs.question(`Digite a senha da sua conta : `)
+    return pedirSenha
+}
+
+function verificarSenha(senha) {
+    if(contaUsuario.senha === senha) {
+        acessoConcedido = true
+        return console.log(`Olá ${contaUsuario.infoPessoais.nomeCompleto}. Acessando conta...`)
+    }
+    tentativasDeSenha --
+    return console.log(`Senha incorreta!${tentativasDeSenha} tentativas restantes.`)
+}   
+function acessarConta() {
+    do{
+        do{
+            verificarConta(pedirConta())
+        }while(contaUsuario === null)
+    }while(contaBloqueada(contaUsuario))
+    do{
+        verificarSenha(pedirSenha())
+    }while(!acessoConcedido && tentativasDeSenha > 0)
+}
+
+function acessarOpcaoPrincipal(escolha) {
+    switch(escolha) {
+        case 1:
+            sacar()
+            break
+        case 2:
+            depositar()
+            break
+        case 0:
+            console.log(`Saindo...`)
+            break
+    }
+}
+
+function exibirMenuPrincipal() {
+    console.log("=====================================")
+    console.log("    BEM-VINDO AO BANCO DO BOSTIL")
+    console.log("=====================================")
+    console.log(`Olá ${contaUsuario.infoPessoais.nome}`)
+    console.log(`Saldo disponível: R$ ${(contaUsuario.saldo).toFixed(2)}`)
+    console.log("1 - Sacar")
+    console.log("2 - Depositar")
+    console.log("0 - Sair da Conta")
+    console.log("=====================================")
+}
+
+function iniciar() {
+    do{
+        let opcao = null
+        exibirMenuInicial()
+        acessarOpcaoInicial(escolhaDeValor(0,2))
+        if(acessoConcedido) {
+            exibirMenuPrincipal()
+            acessarOpcaoPrincipal(escolhaDeValor(0,2))
+        } 
+    }while(opcao !== 0)
+}
+
+iniciar()
